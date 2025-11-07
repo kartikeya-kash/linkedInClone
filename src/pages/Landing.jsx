175509423dff755d461b2ac6e10157cea36af1bc
Kartikeya <kartikeya.anjul@gmail.com>
2025-11-07 21:32:01 +0530

    added Landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+function Landing() {
+  return (
+    <>
+      <h1>Landing Page</h1>
+      <button style={{ marginTop: "10px" }}>Lets get started</button>
+    </>
+  );
+}
+
+export default Landing;
