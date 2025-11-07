05b04b3881918108771a56030f7ffe2d456899f5
Kartikeya <kartikeya.anjul@gmail.com>
2025-11-07 21:50:44 +0530

    created form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+
 function Signup() {
-  return <>Sign UP Page</>;
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <>
+      <h1>Sign Up Page</h1>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // stop page reload
+          alert(formData.email);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={formData.fullName}
+          onChange={(e) =>
+            setFormData({ ...formData, fullName: e.target.value })
+          }
+        />
+        <br />
+        <input
+          type="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </>
+  );
 }
 
 export default Signup;
